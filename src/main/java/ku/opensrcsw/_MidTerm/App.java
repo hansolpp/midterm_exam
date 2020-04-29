8c1da034be9462e7a4c232ae932c73411caa1783
hansolpp <hansolmin3@gmail.com>
2020-04-29 10:23:36 +0900

    version_1

--- src/main/java/ku/opensrcsw/_MidTerm/App.java
+++ src/main/java/ku/opensrcsw/_MidTerm/App.java
@@ -3,12 +3,17 @@ package ku.opensrcsw._MidTerm;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject; //JSON객체를 만드는데 사용
 import org.json.simple.parser.JSONParser; //JSON객체를 파싱해오는데 사용
 import org.json.simple.parser.ParseException; //예외처리
+import midterm.problem2.RegularExpression;
+import java.util.StringTokenizer;
+
 
 public class App 
 {
@@ -28,14 +33,27 @@ public class App
    	 	// 찾고자 하는거 담음
    	 	String looking_for = consol.next();
    	 	
-   	 	// 출력하기
-   	 	for(int i = 0; i < infoArray.size(); i++) {
-   	 		JSONObject itemObject = (JSONObject) infoArray.get(i);
-   	 		String temp = (String) itemObject.get("item");
-   	 		if(temp.contains(looking_for)) {
-   	 			System.out.println("item " + i +": " + itemObject.get("item"));
+   	 	RegularExpression re = new RegularExpression();
+   	 	StringTokenizer tokens = new StringTokenizer(looking_for, "|");
+
+   	 	// 중복 제거를 위해
+   	 	List<String> my_list = new ArrayList<>();
+   	 	
+   	 	while(tokens.hasMoreTokens()) {
+   	 		String split_string = tokens.nextToken();
+   	 		// 출력하기
+   	 		for(int i = 0; i < infoArray.size(); i++) {
+   	 			
+   	 			if(my_list.contains(Integer.toString(i))) continue;
+   	 			else my_list.add(Integer.toString(i));
+   	 			
+   	 			JSONObject itemObject = (JSONObject) infoArray.get(i);
+   	 			String temp = (String) itemObject.get("item");
+   	 			if(temp.contains(split_string)) {
+   	 				System.out.println("item " + i +": " + itemObject.get("item"));
+   	 			}
    	 		}
-   	 	}
         consol.close();
+   	 	}
     }
 }
